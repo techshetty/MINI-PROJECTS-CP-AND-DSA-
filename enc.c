0decfd59af74dc09153f2ae452d6a8663af2bbc8
Pratheek G Shetty <83195025+techshetty@users.noreply.github.com>
2023-07-06 14:18:56 +0530

    Update enc.c

--- enc.c
+++ enc.c
@@ -2,10 +2,11 @@
 void enc(char s[],int k){
 for(int i=0;s[i]!='\0';i++){
     s[i]+=k;
+    if(s[i]>'z') s[i]-=26;
 }
 }
 void main(){
-    char s[20]; int k;
+    char s[99]; int k;
     printf("Enter a string:: ");
     gets(s);
     printf("Enter the shift factor: ");
