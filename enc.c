1477081710687ae86929905b340afb31ac056fe4
Pratheek G Shetty <83195025+techshetty@users.noreply.github.com>
2023-07-06 14:30:06 +0530

    added comments and decode function

--- enc.c
+++ enc.c
@@ -1,17 +1,29 @@
 #include<stdio.h>
+//encode funtion
 void enc(char s[],int k){
 for(int i=0;s[i]!='\0';i++){
     s[i]+=k;
     if(s[i]>'z') s[i]-=26;
 }
 }
-void main(){
-    char s[99]; int k;
-    printf("Enter a string:: ");
-    gets(s);
+//decode function
+void dec(char s[],int k){
+for(int i=0;s[i]!='\0';i++){
+    s[i]-=k;
+    if(s[i]<'a') s[i]+=26;
+}
+}
+int main(){
+    char s[99],o; int k;
+    printf("Enter a string:: "); 
+    gets(s);    //string input
     printf("Enter the shift factor: ");
-    scanf("%d",&k);
-    enc(s,k);
-    printf("Encoded string: %s",s);
-
+    scanf("%d",&k); //shift fator input
+    printf("Encode or Decode? (e or d): ");
+    scanf(" %c",&o); //user choice input
+    if(o=='e') enc(s,k);
+    else if(o=='d') dec(s,k);
+    else {printf("Invalid option"); return 0;}
+    printf("Encoded string: %s",s); //printing the modified string
+    return 0;
 }
