6773a4ec4ff1921bb1c59fedf41f71bd17e0372a
Pratheek G Shetty <83195025+techshetty@users.noreply.github.com>
2023-11-04 21:03:20 +0530

    Add files via upload

---
+++ morsetxt.java
@@ -0,0 +1,59 @@
+import java.util.*;
+public class morsetxt{
+    public static void main(String[] args) {
+       Scanner sc= new Scanner(System.in);
+       HashMap<Character, String> cm = new HashMap<>() {{
+        put('A', ".-"); put('B', "-..."); put('C', "-.-."); put('D', "-.."); put('E', ".");
+        put('F', "..-."); put('G', "--."); put('H', "...."); put('I', ".."); put('J', ".---");
+        put('K', "-.-"); put('L', ".-.."); put('M', "--"); put('N', "-."); put('O', "---");
+        put('P', ".--."); put('Q', "--.-"); put('R', ".-."); put('S', "..."); put('T', "-");
+        put('U', "..-"); put('V', "...-"); put('W', ".--"); put('X', "-..-"); put('Y', "-.--");
+        put('Z', "--..");put('0', "-----"); put('1', ".----"); put('2', "..---"); put('3', "...--"); put('4', "....-");
+        put('5', "....."); put('6', "-...."); put('7', "--..."); put('8', "---.."); put('9', "----.");
+        put(' '," ");
+    }};
+    HashMap<String, Character> mc = new HashMap<>() {{
+        put(".-", 'A'); put("-...", 'B'); put("-.-.", 'C'); put("-..", 'D'); put(".", 'E');
+        put("..-.", 'F'); put("--.", 'G'); put("....", 'H'); put("..", 'I'); put(".---", 'J');
+        put("-.-", 'K'); put(".-..", 'L'); put("--", 'M'); put("-.", 'N'); put("---", 'O');
+        put(".--.", 'P'); put("--.-", 'Q'); put(".-.", 'R'); put("...", 'S'); put("-", 'T');
+        put("..-", 'U'); put("...-", 'V'); put(".--", 'W'); put("-..-", 'X'); put("-.--", 'Y');
+        put("--..", 'Z');put("-----", '0'); put(".----", '1'); put("..---", '2'); put("...--", '3'); put("....-", '4');
+        put(".....", '5'); put("-....", '6'); put("--...", '7'); put("---..", '8'); put("----.", '9');
+        put(" ", ' ');
+    }};
+        System.out.println("MENU");
+        System.out.println("Type 1 to convert Text to Morse code: ");
+        System.out.println("Type 2 to convert Morse code to text: ");
+        System.out.println("Type 3 to exit: ");;
+        int x=sc.nextInt();
+        sc.nextLine();
+        if(x==1) {
+            System.out.println("Enter the text you want to convert: ");
+            String txt =sc.nextLine().toUpperCase();
+            String morse="";
+            for(char c: txt.toCharArray()){
+                morse+=cm.get(c)+" ";
+            }
+            System.out.printf("%s in morse code is: %s",txt,morse);
+        }
+        else if(x==2){
+            System.out.println("Enter the morse code you want to convert: ");
+            String mrs =sc.nextLine();
+            String fin="";
+            String[] mrw= mrs.split(" ");
+            for(String c: mrw){
+               String s=(""+mc.getOrDefault(c,' '));
+                fin+=s;
+            }
+            System.out.printf("%s in text is: %s",mrs,fin);
+            
+        }
+        else{
+            System.out.println("EXITING...");
+        }
+
+
+    }
+
+    }
